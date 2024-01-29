e3b9d7238a8b595d7e955e1a133b2b44c589425f
jthmiller <jeffrey.miller@unh.edu>
2024-01-29 16:33:38 -0500

    sfgsdf

--- gbif/gbif-crossref.sh
+++ gbif/gbif-crossref.sh
@@ -1,19 +1,23 @@
-
-
-gbif-crossref.sh input-gbif.tsv fam-orders-fish.txt > output-gbif.csv
+## gbif-crossref.sh input-gbif.tsv fam-orders-fish.txt > output-gbif.csv
 
 
 gbifin=${1}
 fams=${2}
 outname=${3}
+remove=${4}
 
-gbifin=0015886-230828120925497.csv
-fams=fam-fish.txt
+gbifin=0083785-231120084113126.csv
+fams=fish_family_list.txt
 out='test'
+remove='remove.txt'
+
 
 awk -v FS='\t' -v OFS=',' '{print $6,$7,$8,$9,$10,$12,$14,$34}' ${gbifin} > temp_cleaned_${gbifin}
-grep -f $fams temp_cleaned_${gbifin} | sort | uniq -c | sort -k1 -h > ${out}_family_hit_counts.csv
+grep -f ${remove} -v  temp_cleaned_${gbifin} > filt_temp_cleaned_${gbifin}
+grep -f ${fams} filt_temp_cleaned_${gbifin} | sort | uniq -c | sort -k1 -h > ${out}_family_hit_counts.csv
 awk '$1 > 3 {print $0}' ${out}_family_hit_counts.csv | cut -f5 -d',' | sort > ${out}_gbif_map_intersect_family.txt
+
+
 
 
 
