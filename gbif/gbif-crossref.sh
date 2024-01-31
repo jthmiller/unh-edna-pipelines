2d30688e754769bb67688ee6834de0e470f5a830
jthmiller <jeffrey.miller@unh.edu>
2024-01-31 12:44:58 -0500

    sdf

--- gbif/gbif-crossref.sh
+++ gbif/gbif-crossref.sh
@@ -11,11 +11,12 @@ fams=fish_family_list.txt
 out='test'
 remove='remove.txt'
 
-
 awk -v FS='\t' -v OFS=',' '{print $6,$7,$8,$9,$10,$12,$14,$34}' ${gbifin} > temp_cleaned_${gbifin}
 grep -f ${remove} -v  temp_cleaned_${gbifin} > filt_temp_cleaned_${gbifin}
-grep -f ${fams} filt_temp_cleaned_${gbifin} | sort | uniq -c | sort -k1 -h > ${out}_family_hit_counts.csv
-awk '$1 > 3 {print $0}' ${out}_family_hit_counts.csv | cut -f5 -d',' | sort > ${out}_gbif_map_intersect_family.txt
+grep -f ${fams} filt_temp_cleaned_${gbifin} | sort | uniq -c  | awk '{print $1}' > counts.tmp
+grep -f ${fams} filt_temp_cleaned_${gbifin} | sort | uniq | awk -v FS=',' -v OFS=',' '{print $2,$3,$4,$6,$7,$8}' > taxa.tmp
+paste -d',' counts.tmp taxa.tmp > ${out}_family_hit_counts.csv
+awk '$1 > 3 {print $0}' ${out}_family_hit_counts.csv > ${out}_gbif_map_intersect_family.txt
 
 
 
